f53ba19f8e34165cdc9861435ceb0a469716b0ea
Dmitry <bens1iller@gmail.com>
2019-05-30 18:03:31 +0300

    project

---
+++ my-app/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (<div>
+			<a href='#s' >Home</a>
+			<a href='#s' >News Feed</a>
+			<a href='#s' >Messages</a>
+	</div>)
+}
+
+export default Footer;
---
+++ my-app/src/Technologies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Technologies = () => {
+	return (<div>
+			<a href='#s' >Home</a>
+			<a href='#s' >News Feed</a>
+			<a href='#s' >Messages</a>
+	</div>)
+}
+
+export default Technologies;
