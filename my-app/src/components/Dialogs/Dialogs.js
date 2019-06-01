a8726f08eaf9b9218bce90b7ceb2bcfded5cbccf
Dmitry <bens1iller@gmail.com>
2019-06-02 00:25:48 +0300

    Dialog
    Добавленна вкладка Dialog. Прописаны реакции при изменении.

--- my-app/src/components/Dialogs/Dialogs.js
+++ my-app/src/components/Dialogs/Dialogs.js
@@ -2,8 +2,29 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 
 const Dialogs = () => {
-  return (<div>
-        	Dialogs
+	 return(<div className={classes.dialogs}>
+	  			<div className="dialogItems">
+		        	<div className={classes.dialog + ' ' + classes.active}>
+		        		Dimon
+		        	</div>
+		           	<div className={classes.dialog}>
+		        		Andrey
+		        	</div>
+		           	<div className={classes.dialog}>
+		        		Sveta
+		        	</div>
+		          	<div className={classes.dialog}>
+		        		Sasha
+		        	</div>
+		           	<div className={classes.dialog}>
+		        		Pokemon
+		        	</div>
+		        </div>
+		        <div className={classes.messages}>
+		        	<div className={classes.message}>Hi</div>
+		        	<div className={classes.message}>Go home</div>
+		        	<div className={classes.message}>Be cool</div>
+		        </div>
         </div>)
 }
 
--- my-app/src/components/NawBar/NawBar.js
+++ my-app/src/components/NawBar/NawBar.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import classes from './NawBar.module.css';
+import {NavLink} from 'react-router-dom';
 
 const NawBar = () => {
   return (
         <nav className={classes.sidebar}>
-          <div className={classes.item}><a href='/profile'>Profile</a></div>
-          <div className={classes.item}><a href='/dialogs'>Messages</a></div>
-          <div className={classes.item}><a>News</a></div>
-          <div className={classes.item}><a>Music</a></div>
-          <div className={classes.item}><a>Settings</a></div>
+          <div className={classes.item + ' ' + classes.itemActive}>
+          	<NavLink to='/profile'>Profile</NavLink>
+          </div>
+          <div className={classes.item + ' ' + classes.itemActive}>
+          	<NavLink to='/dialogs'>Messages</NavLink>
+          </div>
+          <div className={classes.item}>
+          	<NavLink>News</NavLink>
+          </div>
+          <div className={classes.item}>
+          	<NavLink>Music</NavLink>
+          </div>
+          <div className={classes.item}>
+          	<NavLink>Settings</NavLink>
+          </div>
         </nav>
-        )
+    )
 }
 
 export default NawBar;
