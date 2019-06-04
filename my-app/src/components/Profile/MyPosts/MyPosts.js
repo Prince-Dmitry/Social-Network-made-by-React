bfd996be628f13bd2b4937c0d2fc6647aff78b53
Dmitry <bens1iller@gmail.com>
2019-06-04 19:26:14 +0300

    Добавил реакцию на кнопку в MyPosts

--- my-app/src/components/Profile/MyPosts/MyPosts.js
+++ my-app/src/components/Profile/MyPosts/MyPosts.js
@@ -15,11 +15,18 @@ const MyPosts = (props) => {
 
   let postElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />);
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+
   return (<div className="classes.postBlock">
             My posts
             <div>
-              <textarea></textarea>
-              <button>Add post</button>
+              <textarea ref={newPostElement}></textarea>
+              <button onClick={ addPost }>Add post</button>
             </div>
             <div className={classes.posts}>
               { postElements }
